f720be253409c160a756588dc940bb816e7ad286
Sofia Hejdenberg <sofia.klein@kentor.se>
2017-06-13 17:20:48 +0200

    Lagt till lite komponenter i app.component.

--- Komponentkartan-demo/app.component.ts
+++ Komponentkartan-demo/app.component.ts
@@ -1,4 +1,5 @@
 ﻿import { Component } from "@angular/core";
+import { ISelectableItem } from "./node_modules/vgr-komponentkartan/component-package/models/selectableItem.model";
 
 @Component({
     selector: "komponentkartan-demo",
@@ -6,7 +7,23 @@
 })
 
 export class KomponentkartanApplicationComponent {
+    buttonDisabled: boolean;
+    buttonSecondaryDisabled: boolean;
+    selectedRadioOption: ISelectableItem;
+    lockMessage: string;
+    saveCancelMessage: string;
 
+    constructor() {
+        this.buttonDisabled = true;
+        this.buttonSecondaryDisabled = true;
+        this.selectedRadioOption = { displayName: "Inget" } as ISelectableItem;
+        this.lockMessage = "Ingen";
+        this.saveCancelMessage = "Ingen";
+    }
+    
+    onSelectedRadioOptionChanged(option: ISelectableItem) {
+        this.selectedRadioOption = option;
+    }
 }
 
 
