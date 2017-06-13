047f8c869285a238c8395ebf35e701b50909e184
Caroline Bornsjö <caroline.bornsjo@ikaros.se>
2017-06-13 14:57:43 +0200

    Skapat demoprojekt för komponentkartan

---
+++ Komponentkartan-demo/app.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: "komponentkartan-demo",
+    template:`<vgr-text-button>Klicka här</vgr-text-button>`
+})
+
+export class KomponentkartanApplicationComponent {
+
+}
+
+
---
+++ Komponentkartan-demo/app.module.ts
@@ -0,0 +1,22 @@
+﻿import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core"
+import { BrowserModule, Title } from "@angular/platform-browser"
+import { FormsModule } from "@angular/forms";
+
+import { KomponentkartanApplicationComponent } from "./app.component"
+import { KomponentkartanModule } from "./node_modules/vgr-komponentkartan/component-package/komponentkartan.module";
+
+
+@NgModule({
+    imports: [
+        KomponentkartanModule,
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [
+    ],
+    declarations: [
+        KomponentkartanApplicationComponent
+    ],
+    bootstrap: [KomponentkartanApplicationComponent]
+})
+export class AppModule { }
