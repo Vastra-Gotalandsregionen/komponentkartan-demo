115c4c602f2b1303d6a60c7f39001288d9335df4
perakerberg <per.akerberg@evolvetechnology.se>
2017-10-09 11:01:13 +0200

    Sida för låsknapp

--- src/app/lockbutton/lockbutton.component.ts
+++ src/app/lockbutton/lockbutton.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lockbutton.component.scss']
 })
 export class LockbuttonComponent implements OnInit {
-
+  lockMessage: string;
   constructor() { }
 
   ngOnInit() {
