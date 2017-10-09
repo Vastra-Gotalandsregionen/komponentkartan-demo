5d7c5d770b26b5e69d00d1ba0b7cc989d1f10dc6
Caroline Bornsjö <caroline.bornsjo@ikaros.se>
2017-10-09 13:08:50 +0200

    lagt till månadskalendern i demon

--- src/app/monthpicker/monthpicker.component.ts
+++ src/app/monthpicker/monthpicker.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monthpicker.component.scss']
 })
 export class MonthpickerComponent implements OnInit {
-
+  selectedDate: Date;
+  preselectedDate: Date = new Date(2015, 0, 13);
+  maxDate: Date = new Date(2018, 7, 1);
   constructor() { }
 
   ngOnInit() {
