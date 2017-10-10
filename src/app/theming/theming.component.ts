2f8be2152a43c9146ce2155e454adb9c557884ca
perakerberg <per.akerberg@evolvetechnology.se>
2017-10-10 08:29:36 +0200

    Temahantering

--- src/app/theming/theming.component.ts
+++ src/app/theming/theming.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ISelectableItem } from '../../../node_modules/vgr-komponentkartan/component-package/models/selectableItem.model';
 
 @Component({
   selector: 'app-theming',
@@ -7,17 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThemingComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  themes: ISelectableItem[];
+  constructor() {
+    this.themes = [
+      { displayName: 'Neutralt', id: 'theme--neutral' } as ISelectableItem,
+      { displayName: 'Blått', id: 'theme--blue' } as ISelectableItem,
+      { displayName: 'Rött', id: 'theme--red' } as ISelectableItem,
+      { displayName: 'Grönt', id: 'theme--green' } as ISelectableItem
+    ] as ISelectableItem[];
   }
 
-  setTheme(theme: string) {
-    document.getElementById('theme-root').classList.remove('theme--default');
+  ngOnInit() {
+    this.themes.forEach(x => x.selected = this.isThemeActive(x.id));
+  }
+
+  themeChanged(theme: ISelectableItem) {
+    document.getElementById('theme-root').classList.remove('theme--neutral');
     document.getElementById('theme-root').classList.remove('theme--blue');
     document.getElementById('theme-root').classList.remove('theme--red');
     document.getElementById('theme-root').classList.remove('theme--green');
-    document.getElementById('theme-root').classList.add('theme--' + theme);
+    document.getElementById('theme-root').classList.add(theme.id);
+  }
+
+  isThemeActive(themeName: string): boolean {
+    return document.getElementById('theme-root').classList.contains(themeName);
   }
 
 }
