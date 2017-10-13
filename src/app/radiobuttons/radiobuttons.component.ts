11230bcb59ee4538629c0cbf4366195c10c3e992
perakerberg <per.akerberg@evolvetechnology.se>
2017-10-13 13:48:26 +0200

    Dokumenterat radio group

--- src/app/radiobuttons/radiobuttons.component.ts
+++ src/app/radiobuttons/radiobuttons.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ISelectableItem } from '../../../node_modules/vgr-komponentkartan/component-package/models/selectableItem.model';
+import { NotificationIcon } from '../../../node_modules/vgr-komponentkartan/component-package/models/notificationIcon.model';
+import { NotificationType } from '../../../node_modules/vgr-komponentkartan/component-package/models/notificationType.model';
 @Component({
   selector: 'app-radiobuttons',
   templateUrl: './radiobuttons.component.html',
   styleUrls: ['./radiobuttons.component.scss']
 })
 export class RadiobuttonsComponent implements OnInit {
-
   selectedRadioOption: ISelectableItem;
-  @Input() exempelOption: string = "[{id:'1', displayName:'Ett'},{id:'2', displayName:'Två'},{id:'3', displayName:'Tre'}, {id:'4', displayName:'Fyra', disabled:true}]";
+  notificationTypes = NotificationType;
+  notificationIcons = NotificationIcon;
   constructor() { }
 
   ngOnInit() {
@@ -16,9 +18,6 @@ export class RadiobuttonsComponent implements OnInit {
 
   onSelectedRadioOptionChanged(option: ISelectableItem) {
     this.selectedRadioOption = option;
-  }
-  consoleLog(logText: string) {
-    console.log(logText);
   }
 
 }
