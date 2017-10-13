1101408c35f066be30d30ddab7c0bdc918976e7b
mijokoletic <mijo.koletic@kentor.se>
2017-10-13 12:45:56 +0200

    Mallen för dokumentationen klar

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Output } from '@angular/core';
-import { RowNotification } from '../../../node_modules/vgr-komponentkartan/component-package/models/rowNotification.model';
-import { NotificationType } from '../../../node_modules/vgr-komponentkartan/component-package/models/notificationType.model';
-import { NotificationIcon } from '../../../node_modules/vgr-komponentkartan/component-package/models/notificationIcon.model';
+
 
 @Component({
   selector: 'app-checkbox',
@@ -10,14 +8,7 @@ import { NotificationIcon } from '../../../node_modules/vgr-komponentkartan/comp
 })
 export class CheckboxComponent implements OnInit {
 
-  // Mest exempel på hur man får en permanent notis i panelen. Du som ser detta, ta bort denna kommentar och bortkommenterade koden under. Checkboxen behöver ingen notis.
-  // @Output() oversiktnotis;
-  // Du sätter propertyn i html-filen här: <vgr-expandable-container [expanded]="true" [notification]="oversiktnotis">
-  constructor() {
-
-    // Mest exempel på hur man får en permanent notis i panelen. Du som ser detta, ta bort denna kommentar och bortkommenterade koden under. Checkboxen behöver ingen notis.
-    // this.oversiktnotis = { icon: NotificationIcon.ExclamationRed, type: NotificationType.Permanent, message: "Something something, darkside. Something something, complete." } as RowNotification;
-  }
+  constructor() { }
 
   ngOnInit() {
   }
