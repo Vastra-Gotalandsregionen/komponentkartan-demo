6bd53f6df70b2c365f92797fdf46e1f49f9d4f55
perakerberg <per.akerberg@evolvetechnology.se>
2017-10-16 10:22:12 +0200

    Dokumentererat Monthpicker

--- src/app/monthpicker/monthpicker.component.ts
+++ src/app/monthpicker/monthpicker.component.ts
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monthpicker.component.scss']
 })
 export class MonthpickerComponent implements OnInit {
-  selectedDate: Date;
+  lastSelectedDate: Date;
   preselectedDate: Date = new Date(2015, 0, 13);
   maxDate: Date = new Date(2018, 7, 1);
+  firstOfMay2016 = new Date(2016, 4, 1);
+  lastOfMarch2019 = new Date(2019, 2, 31);
   constructor() { }
 
   ngOnInit() {
