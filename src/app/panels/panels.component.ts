7efad11a107f86327d3690af0e899f2ca0d42b40
mijokoletic <mijo.koletic@kentor.se>
2017-11-03 14:44:45 +0100

    panel-sidan, anpassa  efter nya listan.

--- src/app/panels/panels.component.ts
+++ src/app/panels/panels.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ColumnWidth } from 'vgr-komponentkartan';
 
 @Component({
   selector: 'app-panels',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panels.component.scss']
 })
 export class PanelsComponent implements OnInit {
-
+  columnWidth = ColumnWidth;
   constructor() { }
 
   ngOnInit() {
