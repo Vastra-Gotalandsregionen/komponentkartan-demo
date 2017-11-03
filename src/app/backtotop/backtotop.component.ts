18914a4c3698299155b40d686b42b3c9bd233e3a
perakerberg <per.akerberg@evolvetechnology.se>
2017-11-03 13:54:50 +0100

    Uppdaterat back to top med nytt listutseende

--- src/app/backtotop/backtotop.component.ts
+++ src/app/backtotop/backtotop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ColumnWidth } from 'vgr-komponentkartan';
 
 @Component({
   selector: 'app-backtotop',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./backtotop.component.scss']
 })
 export class BacktotopComponent implements OnInit {
-
+  columnWidth = ColumnWidth;
   constructor() { }
 
   ngOnInit() {
