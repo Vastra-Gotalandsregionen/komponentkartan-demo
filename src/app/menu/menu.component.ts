aa3759d336815b86b4b7da33aadec9f08b246b9f
mijokoletic <mijo.koletic@kentor.se>
2017-11-23 18:17:52 +0100

    Menyn, la till ett första kodexempel

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IMenu, IMenuItem, IMenuGroup } from 'vgr-komponentkartan';
+import { HtmlEncodeService } from '../html-encode.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  exampleCodeSimpleMeny = ` this.singleMenu = [
+    {
+        title: 'En nivå',
+        groups: [
+            {
+                order: '0',
+                menuItems: [
+                    { title: 'Sida 1', url: '/1' } as IMenuItem,
+                    { title: 'Sida 2', url: '/2' } as IMenuItem,
+                    { title: 'Sida 3', url: '/3' } as IMenuItem
+                ] as IMenuItem[]
+            } as IMenuGroup
+        ] as IMenuGroup[]
+    } as IMenu,
+] as IMenu[];`;
+  exampleCodeSimpleMenyMarkup: string;
+  constructor(htmlEncoder: HtmlEncodeService) {
+    this.exampleCodeSimpleMenyMarkup = htmlEncoder.prepareHighlightedSection(this.exampleCodeSimpleMeny, 'typescript');
+  }
 
   ngOnInit() {
   }
