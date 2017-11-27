42f3855771976d6ce19f49cc2fc50838f6b09ada
perakerberg <per.akerberg@evolvetechnology.se>
2017-11-27 13:02:16 +0100

    Exempel på låst/upplåst kort med komponenter (dropdown, input, kalender) inkl span.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -8,27 +8,42 @@ import { HtmlEncodeService } from '../html-encode.service';
 })
 export class CardComponent implements OnInit {
 
+  birthDate: Date;
   cardExample = ` <vgr-card>
   <div class="card__top flex-row">
-    <!--Ange klassen .card__top för att sätta innehållet i toppen på kortet -->
-    <vgr-button class="flex-right" [secondary]="true">Åtgärd</vgr-button>
+    <vgr-save-cancel class="flex-right" [secondary]="true" (unlock)="unlocked = true" (save)="unlocked = false"
+        (cancel)="unlocked = false"></vgr-save-cancel>
   </div>
-  <!-- Ett kort består av en eller flera kortsektioner -->
-  <vgr-card-section class="card__left" [title]="'Rubrik'" [subtitle]="'vänster 1'">
-    <!-- En kortsektion med klassen .card__left hamnar till vänster -->
+  <vgr-card-section class="card__left" [expanded]="true" [title]="'Rubrik'" [subtitle]="'Exempel med kontroller'" [readonly]="!unlocked">
+    <div class="title-value-layout">
+      <vgr-title-value [title]="'Förnamn'" [slim]="!unlocked">
+        <vgr-input [value]="'Per-Åke'" [readonly]="!unlocked"></vgr-input>
+      </vgr-title-value>
+      <vgr-title-value [title]="'Efternamn'" [slim]="!unlocked">
+        <vgr-input [value]="'Berg'" [readonly]="!unlocked"></vgr-input>
+      </vgr-title-value>
+      <vgr-title-value [title]="'Boende'" [slim]="!unlocked">
+        <vgr-dropdown [values]="['Hus','Lägenhet','Kartong']" [selectedValue]="'Hus'" [readonly]="!unlocked"></vgr-dropdown>
+      </vgr-title-value>
+      <vgr-title-value [title]="'Födelsedatum'" [slim]="!unlocked">
+        <vgr-datepicker [selectedDate]="birthDate" [readonly]="!unlocked"></vgr-datepicker>
+      </vgr-title-value>
+      <vgr-title-value [title]="'Serienummer'" [slim]="!unlocked">
+        <span>7a787b27-c2cc-441f-a732</span>
+      </vgr-title-value>
+    </div>
+  </vgr-card-section>
+  <vgr-card-section class="card__left" [expanded]="true" [title]="'Rubrik'" [subtitle]="'vänster 2'">
     <span>Innehåll</span>
   </vgr-card-section>
-  <vgr-card-section class="card__left" [title]="'Rubrik'" [subtitle]="'vänster 2'">
+  <vgr-card-section class="card__right" [expanded]="false" [title]="'Rubrik'" [subtitle]="'Höger 1'">
     <span>Innehåll</span>
-  </vgr-card-section>
-  <vgr-card-section class="card__right" [expanded]="true" [title]="'Rubrik'" [subtitle]="'Höger 1'" [readonly]="false">
-    <!-- En kortsektion med klassen .card__right hamnar till höger -->
-    <div>Innehåll</div>
   </vgr-card-section>
 </vgr-card>`;
   cardExampleMarkup: string;
   constructor(htmlEncoder: HtmlEncodeService) {
     this.cardExampleMarkup = htmlEncoder.prepareHighlightedSection(this.cardExample);
+    this.birthDate = new Date(1976, 5, 17);
   }
 
   ngOnInit() {
