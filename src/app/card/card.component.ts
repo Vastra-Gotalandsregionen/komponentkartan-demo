1a56592e6b741eded129f90bf2411666e45ceb89
john <john.pettersson@sigma.se>
2017-11-29 16:43:22 +0100

    upddaterat dokumentation för card fullwidth

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -12,81 +12,91 @@ export class CardComponent implements OnInit {
   cardExampleMarkup2: string;
 
   birthDate: Date;
-  cardExample = ` <vgr-card [mode]="2">
-  <div class="card__top flex-row">
-    <vgr-save-cancel class="flex-right" [secondary]="true"
-      (unlock)="unlocked = true" (save)="unlocked = false" (cancel)="unlocked = false"></vgr-save-cancel>
-  </div>
-  <vgr-card-section class="card__left" [expanded]="true" [title]="'Rubrik'" [subtitle]="'Personuppgifter'" [readonly]="!unlocked">
-    <div class="title-value-layout">
-      <vgr-title-value [title]="'Förnamn'" [slim]="!unlocked">
-        <vgr-input [value]="'Per-Åke'" [readonly]="!unlocked"></vgr-input>
-      </vgr-title-value>
-      <vgr-title-value [title]="'Efternamn'" [slim]="!unlocked">
-        <vgr-input [value]="'Berg'" [readonly]="!unlocked"></vgr-input>
-      </vgr-title-value>
-      <vgr-title-value [title]="'Boende'" [slim]="!unlocked">
-        <vgr-dropdown [values]="['Hus','Lägenhet','Kartong']" [selectedValue]="'Hus'" [readonly]="!unlocked"></vgr-dropdown>
-      </vgr-title-value>
-      <vgr-title-value [title]="'Födelsedatum'" [slim]="!unlocked">
-        <vgr-datepicker [selectedDate]="birthDate" [readonly]="!unlocked"></vgr-datepicker>
-      </vgr-title-value>
-      <vgr-title-value [title]="'Serienummer'" [slim]="!unlocked">
-        <span>7a787b27-c2cc-441f-a732</span>
-      </vgr-title-value>
-    </div>
-  </vgr-card-section>
-  <vgr-card-section class="card__right" [expanded]="true" [title]="'Rubrik'" [subtitle]="'Adressuppgifter'">
-    <div class="title-value-layout ">
-      <vgr-title-value [title]="'Kommun'" [slim]="true">
-        <span>Vänersborg</span>
-      </vgr-title-value>
-      <vgr-title-value [title]="'Besöksaddress'" [slim]="true">
-        <span>Duellvägen
-          <br>462 60
-          <br>Vänersborg
-        </span>
-      </vgr-title-value>
-      <vgr-title-value [title]="'Geokod (PT90)'" [slim]="true">
-        <span>x: 6471784</span>
-      </vgr-title-value>
-    </div>
-  </vgr-card-section>
-  <vgr-card-section class="card__left" [expanded]="false" [title]="'Rubrik'" [subtitle]="'Övrig information'">
-    <span>Innehåll</span>
-  </vgr-card-section>
+  cardExample = `  <vgr-card>
+  <vgr-card-header>
+    <vgr-save-cancel class="flex-right" [secondary]="true" (unlock)="unlocked = true" (save)="unlocked = false" (cancel)="unlocked = false"></vgr-save-cancel>
+  </vgr-card-header>
+  <vgr-card-column>
+    <vgr-card-header>
+      <h1>Första Kollumnen</h1>
+    </vgr-card-header>
+    <vgr-card-section [expanded]="true" [title]="'Rubrik'" [subtitle]="'Personuppgifter'" [readonly]="!unlocked">
+      <div class="title-value-layout">
+        <vgr-title-value [title]="'Förnamn'" [slim]="!unlocked">
+          <vgr-input [value]="'Per-Åke'" [readonly]="!unlocked"></vgr-input>
+        </vgr-title-value>
+        <vgr-title-value [title]="'Efternamn'" [slim]="!unlocked">
+          <vgr-input [value]="'Berg'" [readonly]="!unlocked"></vgr-input>
+        </vgr-title-value>
+        <vgr-title-value [title]="'Boende'" [slim]="!unlocked">
+          <vgr-dropdown [values]="['Hus','Lägenhet','Kartong']" [selectedValue]="'Hus'" [readonly]="!unlocked"></vgr-dropdown>
+        </vgr-title-value>
+        <vgr-title-value [title]="'Födelsedatum'" [slim]="!unlocked">
+          <vgr-datepicker [selectedDate]="birthDate" [readonly]="!unlocked"></vgr-datepicker>
+        </vgr-title-value>
+        <vgr-title-value [title]="'Serienummer'" [slim]="!unlocked">
+          <span>7a787b27-c2cc-441f-a732</span>
+        </vgr-title-value>
+      </div>
+    </vgr-card-section>
+    <vgr-card-section [expanded]="true" [title]="'Rubrik'" [subtitle]="'Adressuppgifter'">
+      <div class="title-value-layout ">
+        <vgr-title-value [title]="'Kommun'" [slim]="true">
+          <span>Vänersborg</span>
+        </vgr-title-value>
+        <vgr-title-value [title]="'Besöksaddress'" [slim]="true">
+          <span>Duellvägen
+            <br>462 60
+            <br>Vänersborg
+          </span>
+        </vgr-title-value>
+        <vgr-title-value [title]="'Geokod (PT90)'" [slim]="true">
+          <span>x: 6471784</span>
+        </vgr-title-value>
+      </div>
+    </vgr-card-section>
+  </vgr-card-column>
+  <vgr-card-column>
+    <vgr-card-header>
+      <h1>Andra Kollumnen</h1>
+    </vgr-card-header>
+    <vgr-card-section [expanded]="false" [title]="'Rubrik'" [subtitle]="'Övrig information'">
+      <span>Innehåll</span>
+    </vgr-card-section>
+  </vgr-card-column>
 </vgr-card>`;
 
-  cardExample2 = `<vgr-card [mode]="1">
-  <div class="card__top flex-row">
-    <vgr-save-cancel class="flex-right" [secondary]="true"
-      (unlock)="unlocked = true" (save)="unlocked = false" (cancel)="unlocked = false"></vgr-save-cancel>
-  </div>
-  <vgr-card-section [expanded]="true" [title]="'Rubrik'" [subtitle]="'Exempel med kontroller'" [readonly]="!unlocked">
-    <div class="title-value-layout">
-      <vgr-title-value [title]="'Förnamn'" [slim]="!unlocked">
-        <vgr-input [value]="'Per-Åke'" [readonly]="!unlocked"></vgr-input>
-      </vgr-title-value>
-      <vgr-title-value [title]="'Efternamn'" [slim]="!unlocked">
-        <vgr-input [value]="'Berg'" [readonly]="!unlocked"></vgr-input>
-      </vgr-title-value>
-      <vgr-title-value [title]="'Boende'" [slim]="!unlocked">
-        <vgr-dropdown [values]="['Hus','Lägenhet','Kartong']" [selectedValue]="'Hus'" [readonly]="!unlocked"></vgr-dropdown>
-      </vgr-title-value>
-      <vgr-title-value [title]="'Födelsedatum'" [slim]="!unlocked">
-        <vgr-datepicker [selectedDate]="birthDate" [readonly]="!unlocked"></vgr-datepicker>
-      </vgr-title-value>
-      <vgr-title-value [title]="'Serienummer'" [slim]="!unlocked">
-        <span>7a787b27-c2cc-441f-a732</span>
-      </vgr-title-value>
-    </div>
-  </vgr-card-section>
-  <vgr-card-section [expanded]="true" [title]="'Rubrik'" [subtitle]="'vänster 2'">
-    <span>Innehåll</span>
-  </vgr-card-section>
-  <vgr-card-section [expanded]="false" [title]="'Rubrik'" [subtitle]="'Höger 1'">
-    <span>Innehåll</span>
-  </vgr-card-section>
+  cardExample2 = `<vgr-card>
+  <vgr-card-header>
+    <vgr-save-cancel class="flex-right" [secondary]="true" (unlock)="unlocked = true" (save)="unlocked = false" (cancel)="unlocked = false"></vgr-save-cancel>
+  </vgr-card-header>
+  <vgr-card-column>
+    <vgr-card-section [expanded]="true" [title]="'Rubrik'" [subtitle]="'Exempel med kontroller'" [readonly]="!unlocked">
+      <div class="title-value-layout">
+        <vgr-title-value [title]="'Förnamn'" [slim]="!unlocked">
+          <vgr-input [value]="'Per-Åke'" [readonly]="!unlocked"></vgr-input>
+        </vgr-title-value>
+        <vgr-title-value [title]="'Efternamn'" [slim]="!unlocked">
+          <vgr-input [value]="'Berg'" [readonly]="!unlocked"></vgr-input>
+        </vgr-title-value>
+        <vgr-title-value [title]="'Boende'" [slim]="!unlocked">
+          <vgr-dropdown [values]="['Hus','Lägenhet','Kartong']" [selectedValue]="'Hus'" [readonly]="!unlocked"></vgr-dropdown>
+        </vgr-title-value>
+        <vgr-title-value [title]="'Födelsedatum'" [slim]="!unlocked">
+          <vgr-datepicker [selectedDate]="birthDate" [readonly]="!unlocked"></vgr-datepicker>
+        </vgr-title-value>
+        <vgr-title-value [title]="'Serienummer'" [slim]="!unlocked">
+          <span>7a787b27-c2cc-441f-a732</span>
+        </vgr-title-value>
+      </div>
+    </vgr-card-section>
+    <vgr-card-section [expanded]="true" [title]="'Rubrik'" [subtitle]="'vänster 2'">
+      <span>Innehåll</span>
+    </vgr-card-section>
+    <vgr-card-section [expanded]="false" [title]="'Rubrik'" [subtitle]="'Höger 1'">
+      <span>Innehåll</span>
+    </vgr-card-section>
+  </vgr-card-column>
 </vgr-card>`;
 
 
