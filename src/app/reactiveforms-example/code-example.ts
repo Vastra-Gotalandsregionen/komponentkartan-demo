1b1a82e70df1a6ccb1736a3c90f312c5d630bd33
Felicia Ringblom <felicia.ringblom@gmail.com>
2017-12-11 21:13:00 +0100

    Uppdatera kodexempel reactive forms

--- src/app/reactiveforms-example/code-example.ts
+++ src/app/reactiveforms-example/code-example.ts
@@ -1,61 +1,82 @@
 export class Examples {
   htmltSimpFormMarkup = `
   <form [formGroup]="userForm">
-    <div class="title-value-layout">
-      <vgr-title-value title="Förnamn">
-        <vgr-input formControlName="firstname" [isInvalid]="userForm.controls.firstname.invalid" 
-        [errormessage]="'Du måste ange ett namn på minst 2 tecken'"></vgr-input>
-      </vgr-title-value>
-      <vgr-title-value title="Efternamn">
-        <vgr-input formControlName="lastname" [isInvalid]="userForm.controls.lastname.invalid" 
-        [errormessage]="'Du måste ange ett namn på minst 2 tecken'"></vgr-input>
-      </vgr-title-value>
-      <vgr-title-value title="Ålder">
-        <vgr-input formControlName="age" [rightAlign]="true" [suffix]="'år'" [isInvalid]="userForm.controls.age.invalid" 
-        [errormessage]="'Felaktig ålder'"></vgr-input>
-      </vgr-title-value>
-      <vgr-title-value title="E-post">
-        <vgr-input formControlName="email" [isInvalid]="userForm.controls.email.invalid" 
-        [errormessage]="'Ange en giltig e-postadress'"></vgr-input>
-      </vgr-title-value>
-    </div>
-    <vgr-button [disabled]="userForm.invalid">Skapa användare</vgr-button>
-  </form>`;
+  <div class="title-value-layout">
+    <vgr-title-value title="Förnamn">
+      <vgr-input formControlName="firstname" [errormessage]="validationMessages.firstname"></vgr-input>
+    </vgr-title-value>
+    <vgr-title-value title="Efternamn">
+      <vgr-input formControlName="lastname" [errormessage]="validationMessages.lastname"></vgr-input>
+    </vgr-title-value>
+    <vgr-title-value title="Ålder">
+      <vgr-input formControlName="age" [rightAlign]="true" [suffix]="'år'" [errormessage]="validationMessages.age"></vgr-input>
+    </vgr-title-value>
+    <vgr-title-value title="E-post">
+      <vgr-input formControlName="email" [errormessage]="validationMessages.email"></vgr-input>
+    </vgr-title-value>
+    <vgr-title-value title="Inkomst">
+      <vgr-input formControlName="salary" [errormessage]="validationMessages.salary" [formatNumber]="true"
+        [nrOfDecimals]="0"></vgr-input>
+    </vgr-title-value>
+  </div>
+  <vgr-button [disabled]="userForm.invalid">Skapa användare</vgr-button>
+</form>`;
 
-  typeScriptSimpleFormMarkup = `import { Component, OnInit } from '@angular/core';
-  import { HtmlEncodeService } from '../html-encode.service';
-  import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-  
-  import { Examples } from './code-example';
-  
+  typeScriptSimpleFormMarkup = `
+  import { Component, OnInit } from '@angular/core';
+  import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+
   @Component({
-      selector: 'app-reactiveformscodeexample',
-      templateUrl: './reactiveforms-example.component.html',
+      templateUrl: './reactiveforms-example.component.html'
   })
   export class ReactiveformsexampleComponent implements OnInit {
       userForm: FormGroup;
-      typeScriptSimpleListMarkup: string;
-      htmlSimpleListMarkup: string;
-      examples: Examples = new Examples();
-  
-      constructor(htmlEncoder: HtmlEncodeService, private fb: FormBuilder) {
-          this.typeScriptSimpleListMarkup =
-              htmlEncoder.prepareHighlightedSection(this.examples.typeScriptSimpleFormMarkup, 'typescript');
-          this.htmlSimpleListMarkup =
-              htmlEncoder.prepareHighlightedSection(this.examples.htmltSimpFormMarkup);
-      }
+      validationMessages = {
+          firstname: {
+              'minlength': 'Namnet måste vara minst 2 tecken',
+          },
+          lastname: {
+              'minlength': 'Namnet måste vara minst 2 tecken',
+          },
+          age: {
+              'invalidNumber': 'Ange en siffra',
+              'min': 'Ange en ålder på minst 18 år',
+              'max': 'Ange en ålder under 120',
+          },
+          email: {
+              'email': 'Ange en giltig e-post',
+          },
+          salary: {
+              'invalidNumber': 'Ange ett giltigt belopp',
+              'required': 'Detta skriver över default meddelandet för obligatoriska fält'
+          }
+      };
+
+      constructor(private fb: FormBuilder) {}
+
       ngOnInit() {
           this.createForm();
       }
-  
+
       createForm() {
           this.userForm = this.fb.group({
               firstname: ['', [Validators.required, Validators.minLength(2)]],
               lastname: ['', [Validators.required, Validators.minLength(2)]],
-              age: ['', [Validators.required, Validators.min(18), Validators.max(120)]],
-              email: ['', [Validators.required, Validators.email]]
+              age: ['', [Validators.required, Validators.min(18), Validators.max(120), validateNumber]],
+              email: ['', [Validators.required, Validators.email]],
+              salary: ['', [Validators.required, validateNumber]]
           });
       }
+  }
+
+  function validateNumber(control: AbstractControl) {
+      const pattern = '^[-,−]{0,1}(\\d{1,3}([,\\s.]\\d{3})*|\\d+)([.,]\\d+)?$';
+
+      const regexp = new RegExp(pattern);
+      if (regexp.test(control.value)) {
+          return null;
+      }
+      return { invalidNumber: true };
   }`;
 }
 
--- src/app/reactiveforms-example/reactiveforms-example.component.ts
+++ src/app/reactiveforms-example/reactiveforms-example.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HtmlEncodeService } from '../html-encode.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 import { Examples } from './code-example';
 
@@ -11,6 +11,27 @@ import { Examples } from './code-example';
 })
 export class ReactiveformsexampleComponent implements OnInit {
     userForm: FormGroup;
+    validationMessages = {
+        firstname: {
+            'minlength': 'Namnet måste vara minst 2 tecken',
+        },
+        lastname: {
+            'minlength': 'Namnet måste vara minst 2 tecken',
+        },
+        age: {
+            'invalidNumber': 'Ange en siffra',
+            'min': 'Ange en ålder på minst 18 år',
+            'max': 'Ange en ålder under 120',
+        },
+        email: {
+            'email': 'Ange en giltig e-post',
+        },
+        salary: {
+            'invalidNumber': 'Ange ett giltigt belopp',
+            'required': 'Detta skriver över default meddelandet för obligatoriska fält'
+        }
+    };
+
     typeScriptSimpleListMarkup: string;
     htmlSimpleListMarkup: string;
     examples: Examples = new Examples();
@@ -29,9 +50,20 @@ export class ReactiveformsexampleComponent implements OnInit {
         this.userForm = this.fb.group({
             firstname: ['', [Validators.required, Validators.minLength(2)]],
             lastname: ['', [Validators.required, Validators.minLength(2)]],
-            age: ['', [Validators.required, Validators.min(18), Validators.max(120)]],
-            email: ['', [Validators.required, Validators.email]]
+            age: ['', [Validators.required, Validators.min(18), Validators.max(120), validateNumber]],
+            email: ['', [Validators.required, Validators.email]],
+            salary: ['', [Validators.required, validateNumber]]
         });
     }
+}
+
+function validateNumber(control: AbstractControl) {
+    const pattern = '^[-,−]{0,1}(\\d{1,3}([,\\s.]\\d{3})*|\\d+)([.,]\\d+)?$';
+
+    const regexp = new RegExp(pattern);
+    if (regexp.test(control.value)) {
+        return null;
+    }
+    return { invalidNumber: true };
 }
 
