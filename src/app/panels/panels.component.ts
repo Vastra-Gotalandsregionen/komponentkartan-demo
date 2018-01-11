991de7a416d8f7bea773813b38ecc0742a5214ff
Felicia Ringblom <felicia.ringblom@stenametall.se>
2018-01-11 21:24:36 +0100

    Gör klart exempel för panel

--- src/app/panels/panels.component.ts
+++ src/app/panels/panels.component.ts
@@ -1,4 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HtmlEncodeService } from '../html-encode.service';
+
+const htmlMarkup = `
+<vgr-panel-container>
+  <vgr-panel width="4" color="green">
+    <!-- html för panel-innehåll -->
+  </vgr-panel>
+  <vgr-panel width="8" color="green">
+    <!-- html för panel-innehåll -->
+  </vgr-panel>
+</vgr-panel-container>
+
+<vgr-panel-container>
+  <vgr-panel width="4" noborder="true">
+    <!-- html för panel-innehåll -->
+  </vgr-panel>
+  <vgr-panel width="8" color="red">
+    <!-- html för panel-innehåll -->
+  </vgr-panel>
+</vgr-panel-container>`;
 
 @Component({
   selector: 'app-panels',
@@ -6,7 +26,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panels.component.scss']
 })
 export class PanelsComponent implements OnInit {
-  constructor() { }
+  private htmlMarkup: string;
+
+  constructor(htmlEncoder: HtmlEncodeService) {
+    this.htmlMarkup =
+      htmlEncoder.prepareHighlightedSection(htmlMarkup);
+  }
 
   ngOnInit() {
   }
