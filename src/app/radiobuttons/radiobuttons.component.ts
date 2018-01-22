a0e74c07df7add10e06d7de3982e85eab1edd4ca
VGREGION\carbo15 <caroline.bornsjo@ikaros.se>
2018-01-22 15:25:46 +0100

    Fixat till exempel för radiobuttons

--- src/app/radiobuttons/radiobuttons.component.ts
+++ src/app/radiobuttons/radiobuttons.component.ts
@@ -9,13 +9,20 @@ export class RadiobuttonsComponent implements OnInit {
   selectedOption: string;
   notificationTypes = NotificationType;
   notificationIcons = NotificationIcon;
+
+  options: Array<SelectableItem<any>> = [];
   constructor() { }
 
   ngOnInit() {
+    this.options = [{ value: 'Val 1', displayName: 'Val 1' }, { value: 'Val 2', displayName: 'Val 2' }, { value: 'Val 3', displayName: 'Val 3' }];
   }
 
   onSelectedRadioOptionChanged(option: string) {
     this.selectedOption = option;
+  }
+
+  rensaVal() {
+    this.options.forEach(o => o.selected = false);
   }
 
 }
