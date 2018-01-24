3246eb67667debe15f63c2a1750951ba675b743a
Caroline Bornsjö <SE2321000131-P000000226420@vgregion.se>
2018-01-24 11:25:06 +0100

    Jobbat med actionpanel och kort

--- src/app/examples-listwithcards/examples-listwithcards.component.ts
+++ src/app/examples-listwithcards/examples-listwithcards.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SortDirection, SortChangedArgs } from 'vgr-komponentkartan'
+import { SortDirection, SortChangedArgs, SelectableItem } from 'vgr-komponentkartan'
 
 @Component({
   selector: 'app-examples-listwithcards',
@@ -10,10 +10,23 @@ export class ExamplesListwithcardsComponent implements OnInit {
   sortDirections = SortDirection;
   exampleData: ExampleUnits[] = [];
   filtertext: string = "";
-  constructor() { this.exampleData = this.initExampleData(); }
+  newUnits: SelectableItem<string>[] = [];
+  itemSelected: boolean = false;
+  addNewUnit: boolean = false;
+  showActionPanel: boolean = false;
+  selectedUnit: string = "";
+  constructor() {
+    this.exampleData = this.initExampleData();
+    this.newUnits = [{ displayName: 'Närhälsan Lerum', value: 'SE2321000131-E000000011801' },
+    { displayName: 'Fredriks Rehab/Massage', value: 'SE2321000131-E000000011802' },
+    { displayName: 'Bvc för alla', value: 'SE2321000131-E000000011803' }]
+
+
+  }
 
   ngOnInit() {
 
+    this.onSortChanged({ key: 'enhet', direction: SortDirection.Ascending } as SortChangedArgs)
   }
 
   getRandomInt(min, max) {
@@ -35,7 +48,7 @@ export class ExamplesListwithcardsComponent implements OnInit {
     const examplenamnd: string[] = ['Göteborgs hälso- och sjukvårdsnämnden', 'Norra hälso- och sjukvårdsnämnden', 'Södra hälso- och sjukvårdsnämnden', 'Västra hälso- och sjukvårdsnämnden', 'Östra hälso- och sjukvårdsnämnden'];
 
 
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= 15; i++) {
       const indexForNames = this.getRandomInt(0, 12);
       const indexForAgare = this.getRandomInt(0, 4);
       const indexForEnhetskod = this.getRandomInt(0, 12);
@@ -52,6 +65,23 @@ export class ExamplesListwithcardsComponent implements OnInit {
     return items;
   }
 
+  onSelectedChanged(selectedItem: string) {
+    console.log(selectedItem);
+    this.itemSelected = true;
+    this.selectedUnit = this.newUnits.find(u => u.value === selectedItem).displayName;
+
+  }
+
+  onActionPanelClose() {
+    this.showActionPanel = false;
+    this.addNewUnit = false;
+
+
+    this.newUnits.forEach(u => u.selected = false);
+    this.itemSelected = false
+    console.log(this.newUnits);
+
+  }
   onSortChanged(event: SortChangedArgs) {
     this.exampleData = this.exampleData.sort((row1, row2) => {
       return row1[event.key] > row2[event.key] ? (event.direction === SortDirection.Ascending ? 1 : -1) :
