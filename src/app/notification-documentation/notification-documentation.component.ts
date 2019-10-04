9da49b3e63e26168b0e919e994bf0d8dcafd8c3f
Synnöve Lysö <synnove.lyso@elicit.se>
2019-10-04 11:14:31 +0200

    Mer dokumentation.

--- src/app/notification-documentation/notification-documentation.component.ts
+++ src/app/notification-documentation/notification-documentation.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification-documentation',
   templateUrl: './notification-documentation.component.html',
   styleUrls: ['./notification-documentation.component.scss']
 })
-export class NotificationDocumentationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class NotificationDocumentationComponent {
 
 }
