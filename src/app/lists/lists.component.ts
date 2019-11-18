678b2c4c1bf9310d1fae73f3e50615f0399f37d1
Fredrik Aronsson <fredrik.e.aronsson@gmail.com>
2019-11-18 16:17:33 +0100

    Uppgradera till Angular 8 / notering om att vgr-list är deprecated

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Examples } from './examples/examples';
 import { HtmlEncodeService } from '../html-encode.service';
+import { RowNotification, NotificationType } from 'vgr-komponentkartan';
 
 @Component({
   selector: 'app-lists',
@@ -10,6 +11,14 @@ import { HtmlEncodeService } from '../html-encode.service';
 export class ListsComponent {
   htmlBasicListStructureMarkup: string;
   examples: Examples = new Examples();
+
+  notification: RowNotification = {
+    icon: { name: 'exclamation-circle', color: 'error', solid: true },
+    type: NotificationType.Permanent,
+    message: 'Använd vår nya komponent vgr-grid i stället då vgr- list är deprecated.',
+    done: true,
+    removeWhenDone: false
+  };
 
   constructor(htmlEncoder: HtmlEncodeService) {
 
