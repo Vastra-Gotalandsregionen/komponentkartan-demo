4724e2b817e622522732d2eb7d669f113fa76d7a
Junior <albertino-junior.lopes-livramento@vgregion.se>
2022-03-16 08:17:53 +0100

    Uppdaterat till ny betaversion samt lagt in exempel på checkboxen

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { CheckboxComponentElement } from 'vgr-komponentkartan';
 
 
 @Component({
@@ -7,10 +9,56 @@ import { Component, OnInit, Output } from '@angular/core';
   styleUrls: ['./checkbox.component.scss']
 })
 export class CheckboxComponent implements OnInit {
+  // @ViewChild('myCheckbox', { read: CheckboxComponentElement }) myCheckbox: CheckboxComponentElement;
+  form: FormGroup;
+  checkboxForm: FormGroup;
+  showValidFormText: boolean = false;
+  formSubmittedCheckboxGroup: boolean;
+  formSubmittedCheckbox: boolean;
+  // checkbox = new FormControl('', Validators.required);
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createForm();
   }
+
+  createForm() {
+    this.form = this.fb.group({
+      checkbox: ['', [Validators.required]]
+    }, { updateOn: 'submit' });
+
+    this.checkboxForm = this.fb.group({
+      checkboxValues: [['Vald'], { validators: [Validators.required]}]
+    }, { updateOn: 'submit' });
+  }
+
+  submitForm() {
+    this.form.controls.checkbox.markAsTouched();
+
+    this.formSubmittedCheckbox = true;
+    if (this.form.valid) {
+      this.showValidFormText = true;
+
+    } else {
+      this.showValidFormText = false;
+    }
+
+  }
+
+  submitCheckboxForm() {
+    this.formSubmittedCheckboxGroup = true;
+    // this.checkboxForm.controls.checkboxValues.markAsTouched();
+    // Do nothing...
+    // this.checkboxForm.reset();
+  }
+
+  resetCheckboxForm() {
+    this.formSubmittedCheckboxGroup = false;
+  }
+
+  // setFocus() {
+  //   this.myCheckbox.focus();
+  // }
 
 }
