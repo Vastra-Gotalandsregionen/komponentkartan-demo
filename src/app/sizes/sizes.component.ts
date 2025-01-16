bc858d1c3a512c48cc878d83b7760871c5eaff3f
Junior Lopes <albertino-junior.lopes-livramento@vgregion.se>
2025-01-16 13:55:21 +0100

    fixat med importer

--- src/app/sizes/sizes.component.ts
+++ src/app/sizes/sizes.component.ts
@@ -10,7 +10,7 @@ export class SizesComponent {
 
   mediaQueryCode = `
   // Importera _settings.sizes.scss från komponentkartan.
-  @import '../../../node_modules/vgr-komponentkartan/assets/partials/_settings.sizes.scss';
+  @use '../../../node_modules/vgr-komponentkartan/assets/partials/_settings.sizes.scss' as size;
 
   // Sätt default-storlek utan media query. Detta är den lilla storleken.
   .sizeInfo {
@@ -23,7 +23,7 @@ export class SizesComponent {
   }
 
   // Definiera media query baserat på variabel för stor storlek.
-  @media screen and (min-width: $desktop-width--medium) {
+  @media screen and (min-width: size.$desktop-width--medium) {
     .sizeInfo {
       &:after {
         content: "Stor";
